5d6bdce2816d53aa6f944f16c7ebbe29fbb3671e
Quike Ramos Fuster <quike@MacBook-Pro-de-Quike.local>
2024-01-04 18:33:00 +0100

    Solucionado añadir listas ok y ko

--- models.py
+++ models.py
@@ -4,10 +4,13 @@ def open_doc():
     doc = open("input.txt", "r")
     return doc
 
-
+def quita_simbolos(texto):
+    simbols=(",.:""[]'`'")
+    return "". join(char for char in texto if char not in simbols)
 
 def read_linea(doc):
-    simbols= (",.;:""[]'`'")
+    lineas= []
+    simbols= (",.:""[]'`'")
     
     for linea in doc:
         linea = linea.split('\n')
@@ -18,74 +21,136 @@ def read_linea(doc):
                 linea=linea.replace(i,"")
 
         linea = linea.split()
-                  
-        green = []
-        red = []
-        blue = []
+        lineas.append(linea)
+      
+    return lineas    
+
+def cuenta_linea(lineas):
+    green = []
+    red = []
+    blue = []
+    suma_jugadasko= []
+    suma_jugadasok = []
+
+    for linea in lineas:
+        linea = linea
         num_juego = int(linea[1])
         
         
+        
         for i in linea:
-            
-            if i == "green":
+    
+            if i == "green" or i == "green;":
                 pos = linea.index(i)
                 pos_num = int(pos)-1
                 num = linea[pos_num]
                 num = int(num)
                 green.append(num)
-                
                 linea.pop(pos)
+                if num > 13:
+                    suma_jugadasko.append(num_juego)
+                    print(num_juego)
+                    print ("Este juego no cumple porque green es:",num)
+                    break
+                else:
+                    if num_juego in suma_jugadasok:
+                        break
+                    else:
+                        suma_jugadasok.append(num_juego)
+                        break
+                
 
-            elif i == "red": 
+            elif i == "red" or i == "red;": 
+                if num_juego == 43:
+                    print("Ahora")
                 pos = linea.index(i)
                 pos_num = int(pos)-1
                 num = linea[pos_num]
                 num = int(num)
                 red.append(num)
+                linea.pop(pos) 
+                if num > 12:
+                    suma_jugadasko.append(num_juego)
+                    print(num_juego)
+                    print ("Este juego no cumple porque red es:",num)
+                    break
+                else:
+                    if num_juego in suma_jugadasok:
+                        break
+                    else:
+                        suma_jugadasok.append(num_juego)
+                        break
                 
-                linea.pop(pos)  
+                
+                
 
-            elif i == "blue":
+            elif i == "blue" or i == "blue;":
                 pos = linea.index(i)
                 pos_num = int(pos)-1
                 num = linea[pos_num]
                 num = int(num)
                 blue.append(num)
-                
                 linea.pop(pos)
+                if num > 14:
+                    suma_jugadasko.append(num_juego)
+                    print(num_juego)
+                    print ("Este juego no cumple porque blue es:",num)
+                    break
+                else:
+                    if num_juego in suma_jugadasok:
+                        break
+                    else:
+                        suma_jugadasok.append(num_juego)
+                        break
 
-        green = sum(green)
-        red = sum(red)
-        blue = sum(blue)            
+            
+        continue
+    
+    print("Juegos que no cumplen son: ",suma_jugadasko)
+    print("Juegos que si cumplen son: ",suma_jugadasok)
+    solucion = sum(suma_jugadasok)
+    print(solucion)
+    return suma_jugadasko, suma_jugadasok  
+        
+    
+        
+        
 
-        print("Juego Nº: ",num_juego)
-        print("green= ",green)
-        print("red= ",red)
-        print("blue= ",blue)
+       
+                 
+        
+                
 
-#revisar esta oprecion que suma jugadas que no cumplen
-        suma_jugadas= []
 
-        if red > 12:
-            suma_jugadas.append(num_juego)
-        elif green > 13:
-            suma_jugadas.append(num_juego)
-        elif blue > 14:
-            suma_jugadas.append(num_juego)
+        
+        
+        
+            
+                
+                
+               
+                   
 
-        print(suma_jugadas)
-         
+    
+    
+
+
+    
+        
              
 
         
 
-    return suma_jugadas
+    
 
 
 
 
 
 doc = open_doc()
-solucion = read_linea(doc)
-print(solucion)
+leer = read_linea(doc)
+contar = cuenta_linea(leer)
+
+
+
 
